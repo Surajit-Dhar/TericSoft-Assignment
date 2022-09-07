6d53f20355442492dfb53579c7b288a442f6ff88
Surajit Dhar <surajitgokarna@gmail.com>
2022-09-07 16:29:01 +0530

    all most

--- src/components/menu.js
+++ src/components/menu.js
@@ -4,7 +4,7 @@ const Menu = [
        image: "https://images.unsplash.com/photo-1620356816556-038ad240c356?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y3ljbGluZyUyMHN0dW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
        name:"Day1",
        category: "cycleing",
-       time: "5.4s",
+       time: "5.4K",
        description: 'Mountain cycleing shunt'
    },
  
@@ -13,7 +13,7 @@ const Menu = [
        image: "https://images.unsplash.com/photo-1561336893-6c53a71d157b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y3ljbGluZyUyMHN0dW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
        name:"Day2",
        category: "cycleing",
-       time: "2.3s",
+       time: "2.3K",
        description: 'Cycleing shunt on Performance'
    },
    {
@@ -21,40 +21,40 @@ const Menu = [
        image: "https://images.unsplash.com/photo-1486166849615-da9f8d217a00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=803&q=80",
        name:"Day3",
        category: "cycleing",
-       time: "6.1s",
+       time: "6.1K",
        description: 'Shunt on Road'
    },
     {
        id: 4,
        image: "https://images.unsplash.com/photo-1606874721914-5553d6b27cc7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YXF1YXRpY3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-       name:"chola",
+       name:"Highlights",
        category: "aquatics",
-       price: "50₹",
-       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+       time: "1.8K",
+       description: 'The self-propulsion of a person through water '
    },
      {
        id: 5,
        image: "https://images.unsplash.com/photo-1606874711241-2e1d6496ac42?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXF1YXRpY3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-       name:"pizza",
+       name:"Motions",
        category: "aquatics",
-       price: "80₹",
-       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+       time: "2.5K",
+       description: 'Swimming is great for shaping the body'
    },
       {
        id: 6,
        image: "https://images.unsplash.com/photo-1565028387289-0beb1187675a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZXF1ZXN0cmlhbiUyMHNwb3J0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-       name:"Non-Veg Thali",
+       name:"Healing rides",
        category: "equestrian",
-       price: "180₹",
-       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+       time: "3.4K",
+       description: 'An analysis of the collision record, riding behaviours and experiences'
    },
        {
        id: 7,
        image: "https://images.unsplash.com/photo-1594419720156-5c9dffd6b412?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDl8fGVxdWVzdHJpYW4lMjBzcG9ydHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-       name:"Sweets",
+       name:"Highlighted Shunt",
        category: "equestrian",
-       price: "60₹",
-       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+       time: "6.4K",
+       description: 'Equestrian Victoria is a non-profit organisation incorporated under the Associations Incorporations Act of Victoria'
    },
        {
        id: 8,
